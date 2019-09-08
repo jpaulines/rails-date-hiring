2c8b1b4281bce0a02f81e117336eb97a3efd7f62
Judith Saragossi <judithsaragossi@gmail.com>
2019-09-08 17:54:05 +0300

    removed email field from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_08_134552) do
+ActiveRecord::Schema.define(version: 2019_09_08_141820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "event_id"
+    t.boolean "approved"
+    t.string "motivation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_bookings_on_event_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.date "date"
+    t.string "amenities"
+    t.string "event_description"
+    t.string "wishlist"
+    t.string "event_image"
+    t.bigint "user_id"
+    t.string "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +49,24 @@ ActiveRecord::Schema.define(version: 2019_09_08_134552) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "city"
+    t.string "description"
+    t.string "profile_image"
+    t.string "height"
+    t.string "strength"
+    t.string "language"
+    t.string "hidden_talent"
+    t.integer "talkativeness"
+    t.integer "funniness"
+    t.string "username"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "events"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "events", "users"
 end
