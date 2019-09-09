e6b1aa8b7ad14eb11e516a6fc3adebc18d7d03fa
Judith Saragossi <judithsaragossi@gmail.com>
2019-09-09 12:29:06 +0300

    update for the show branch

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,11 @@
 class EventsController < ApplicationController
 
+  def show
+    # @event = Event.find(params[:id])
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :location, :date, :amenities, :event_description, :wishlist, :event_image, :cost)
+  end
 end
