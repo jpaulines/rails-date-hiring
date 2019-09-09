0c7e11589084bdd7aa5776b0a5b32d3536af2b7d
Emilie Cazaux <emilie.cazaux.dussous@gmail.com>
2019-09-09 12:29:11 +0300

    update for my branch

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,23 @@
 class EventsController < ApplicationController
 
+  def new
+    @event = Event.new
+    authorize @event
+  end
+
+  def create
+     @event = Event.new(event_params)
+     authorize @event
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render 'new'
+    end
+  end
+
 end
+
+private
+ def event_params
+   params.require(:event).permit(:name, :location, :date, :amenities, :event_description, :wishlist, :event_image, :cost)
+ end
