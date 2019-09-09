400f780e0b23509252567ca12a69e0aafccc2618
Judith Saragossi <judithsaragossi@gmail.com>
2019-09-09 15:06:04 +0300

    updated show with booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,7 @@
 class BookingsController < ApplicationController
+
+  def create
+
+  end
+
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,7 @@ class EventsController < ApplicationController
 
   def show
     # @event = Event.find(params[:id])
+    @booking = Booking.new
   end
 
   private
