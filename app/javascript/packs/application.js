cc224714c2cc51e34dbb839bbf9dd8850bf83a6e
Brian Watroba <brianwatroba@gmail.com>
2019-09-09 14:34:14 +0300

    added agolia places search

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+
+
+// Algolia places
+
+var places = require('places.js');
+places({
+  appId: 'plIAULCHR775',
+  apiKey: '85e799933c00b2920bf320a2eb51b31e',
+  container: document.querySelector('#location-input'),
+  type: 'city',
+});
+
